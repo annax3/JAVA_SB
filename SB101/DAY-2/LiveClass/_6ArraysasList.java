51c4d0025f00808df5607929e5b5d896369318cf
Prashant Anand <99432929+annax3@users.noreply.github.com>
2022-10-14 21:44:25 +0530

    Update _6ArraysasList.java

--- SB101/DAY-2/LiveClass/_6ArraysasList.java
+++ SB101/DAY-2/LiveClass/_6ArraysasList.java
@@ -1,5 +1,21 @@
 package com.LiveClass;
 
-public class _6ArraysasList {
+import java.util.Arrays;
 
+public class _6ArraysasList {
+	public static void main(String [] args)  
+	{  
+	//declaration and initialization of String array  
+	String[] Array={"Scaler","Java","Tutorial"};  
+	Object stringArray = null;
+	System.out.println(Arrays.asList(stringArray));  
+	}  
 }
+/*
+If we have an array of non-primitive data types, we can use Arrays.asList() 
+method to print the array. Arrays.asList() method returns a fixed-size list 
+backed by the specified array and then that ArrayList can be directly printed.
+This method doesn't copy the array elements into a new list, rather it just
+provides a list view to our array. It acts like a list wrapped around an 
+array.
+*/
