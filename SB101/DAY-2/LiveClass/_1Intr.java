fa5fef4a22982ba9a6502b1caf4963bedd237b43
Prashant Anand <99432929+annax3@users.noreply.github.com>
2022-10-16 02:08:06 +0530

    Update _1Intr.java

--- SB101/DAY-2/LiveClass/_1Intr.java
+++ SB101/DAY-2/LiveClass/_1Intr.java
@@ -1,5 +1,17 @@
 package com.LiveClass;
 
+@FunctionalInterface
 public interface _1Intr {
-
+	public static int x =100; // it is by default public static final
+	public abstract void fun1();//it is by default public and abstract whether 
+	//you mention or not
+	
+	public default void fun2() {
+		
+	}
+	public static void fun3() {
+		
+	}
+	
+	public abstract String toString();
 }
